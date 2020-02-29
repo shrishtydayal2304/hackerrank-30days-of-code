b17c597d566eafabb40d7abcaff736a0099f43c5
Shrishty Dayal <60177793+shrishtydayal2304@users.noreply.github.com>
2020-02-29 16:51:13 +0530

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        String k=S.toLowerCase();
+          
+        try
+        {
+            int a=Integer.parseInt(k);
+            System.out.println(a);
+        }
+        catch(Exception ex){  
+          System.out.println("Bad String");
+        }
+     
+    }
+}
